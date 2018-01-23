dcf4d6bf76f379f54f9fa04c36aca0192eed4168
anisfairizza <35678606+anisfairizza@users.noreply.github.com>
2018-01-23 12:31:40 +0800

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -72,6 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<div id="body">
 		Hello World, it is codeigniter
+		This website is about Academic Information System
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
